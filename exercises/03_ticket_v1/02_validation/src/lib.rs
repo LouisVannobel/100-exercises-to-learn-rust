5ddcea62c5b13965df2377cfcd6c8860c47258fd
LouisVannobel <louisvannob@gmail.com>
2024-10-26 15:53:44 +0200

    ex 03 02

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -17,18 +17,39 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        if status != "To-Do" && status != "In Progress" && status !="Done" {
+        /*if status != "To-Do" && status != "In Progress" && status !="Done" {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed.");
         }
         if title.is_empty() || description.is_empty() {
             panic!("The `title` and `description` fields should not be empty.");
         }
-        if title.len() <= 50{
+        //assert!(title.len() < 50, "the `title` should be at most 50 bytes long. actuellement il est de {}", title.len());
+        if title.len() > 50{
             panic!("the `title` should be at most 50 bytes long.")
         }
-        if description.len() >= 500{
+        if description.len() > 500{
             panic!("the `description` should be at most 500 bytes long.")
-        };
+        }*/
+
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+
+        
 
         //todo!();
         Self {
